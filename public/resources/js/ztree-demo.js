37d2fee70f4a9a2be546f5c51545edcdd1750fbd
yjkimpomit <yjkim.pomit@gmail.com>
2025-10-28 11:45:47 +0900

    R01

--- public/resources/js/ztree-demo.js
+++ public/resources/js/ztree-demo.js
@@ -18,15 +18,25 @@ var setting = {
 
 // 샘플 데이터
 var zNodes = [
-	{ id: 1, pId: 0, name: "메뉴 1", open: true },
-	{ id: 11, pId: 1, name: "서브메뉴 1-1" },
-	{ id: 12, pId: 1, name: "서브메뉴 1-2" },
-	{ id: 2, pId: 0, name: "메뉴 2", open: true },
-	{ id: 21, pId: 2, name: "서브메뉴 2-1" },
-	{ id: 211, pId: 21, name: "서브메뉴 2-1-1" },
-	{ id: 212, pId: 21, name: "서브메뉴 2-1-2" },
-	{ id: 22, pId: 2, name: "서브메뉴 2-2" }
+  { id: 1, pId: 0, name: "메뉴 1", open: true },
+
+  // 기존 메뉴 1 하위
+  { id: 11, pId: 1, name: "서브메뉴 1-1" },
+  { id: 12, pId: 1, name: "서브메뉴 1-2" },
+
+  // 기존 메뉴 2 계층을 메뉴1 하위로 이동
+  { id: 2, pId: 1, name: "메뉴 2", open: true },
+  { id: 21, pId: 2, name: "서브메뉴 2-1" },
+  { id: 211, pId: 21, name: "서브메뉴 2-1-1" },
+  { id: 212, pId: 21, name: "서브메뉴 2-1-2" },
+  { id: 22, pId: 2, name: "서브메뉴 2-2" },
+
+  // 기존 메뉴 3 계층도 메뉴1 하위로 이동
+  { id: 3, pId: 1, name: "메뉴 3", open: false, icon: "icons/folder.png" },
+  { id: 31, pId: 3, name: "서브메뉴 3-1", icon: "icons/doc.png" },
+  { id: 32, pId: 3, name: "서브메뉴 3-2", icon: "icons/doc.png" }
 ];
+
 
 function customizeNode(treeId, treeNode) {
     var aObj = $("#" + treeNode.tId + "_a");
